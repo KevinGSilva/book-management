d613daf20e8c6eeb7de7b93bcd251676356af613
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 01:10:43 -0300

    Tela para listagem de livros

---
+++ resources/views/web/books/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.base')
+
+
+@section('content')
+    <div class="row">
+        <div class="card">
+            <div class="card-header">Livros</div>
+            <div class="card-body">
+                <table class="table table-bordered" id="content">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Título</th>
+                            <th>Autor</th>
+                            <th>Data de publicação</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($books as $book)
+                            <tr>
+                                <td>{{ $book->id }}</td>
+                                <td>{{ $book->title }}</td>
+                                <td>{{ $book->author->name }}</td>
+                                <td>{{ $book->published_at->format('d/m/Y') }}</td>
+                                <td>
+                                    <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning">Edit</a>
+                                    <button type="button" class="btn btn-danger" data-id={{ $book->id }}>Delete</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection

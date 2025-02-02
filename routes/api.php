35380534cab78fe4011bfd1309cb362b1990ffb7
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 07:59:29 -0300

    Protegendo rotas api para apenas admin

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Middleware\AdminMiddlewareApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', AdminMiddlewareApi::class])->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 

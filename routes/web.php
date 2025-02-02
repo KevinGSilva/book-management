9e6692b4969a1867b2fd48b14202ec88ff653a3c
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 01:08:34 -0300

    Rotas CRUD de livros

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('books')->namespace('Books')->name('books.')->group(function () {
+    Route::get('/', [App\Http\Controllers\Web\BookController::class, 'index'])->name('index');
+    Route::get('/create', [App\Http\Controllers\Web\BookController::class, 'create'])->name('create');
+    Route::post('/store', [App\Http\Controllers\Web\BookController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [App\Http\Controllers\Web\BookController::class, 'edit'])->name('edit');
+    Route::post('/update', [App\Http\Controllers\Web\BookController::class, 'update'])->name('update');
+    Route::post('/destroy', [App\Http\Controllers\Web\BookController::class, 'destroy'])->name('destroy');
 });

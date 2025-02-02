20eccc91130fad94e2ad98725c2a1d017be85b58
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 01:41:01 -0300

    Requisicao ajax para atualizar livro

--- resources/views/web/books/form.blade.php
+++ resources/views/web/books/form.blade.php
@@ -58,6 +58,55 @@
                 });
             }
         });
+
+        $("#form-update").validate({
+            errorClass: "forminputerror",
+            rules : {},
+            errorPlacement: function(error, element)
+            {
+                error.insertAfter(element);
+            },
+            submitHandler: function(form)
+            {
+                $('#btn-submit').attr('disabled', true)
+
+                const formData = getFormData()
+
+                $.ajax(
+                {
+                    url: '{{ route('books.update') }}',
+                    type: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                }).done(function (response) {
+                    if (response.success) {
+                        Swal.fire({
+                            title: 'Livro',
+                            text: 'Livro atualizado com sucesso',
+                            type: 'success',
+                            icon: 'success',
+                        }).then(() => {
+                            window.location.href = '{{ route("books.index") }}'
+                        })
+                    }
+                }).fail(function (err) {
+                    let message = 'Erro ao atualizar o livro'
+                    if (err.responseJSON && err.responseJSON.errors) {
+                        console.log('err.responseJSON.errors', err.responseJSON.errors)
+                        message = Object.values(err.responseJSON.errors)[0].join('\n')
+                    }
+                    Swal.fire({
+                        title: 'Ops',
+                        text: message,
+                        type: 'error',
+                        icon: 'error',
+                    })
+                }).always(function () {
+                    $('#btn-submit').attr('disabled', false)
+                });
+            }
+        });
     });
 </script>
 @endsection

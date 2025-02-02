c208418d0bb57e3a4d0557609a44ea8f47e8c375
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 01:08:13 -0300

    Grupo de rotas de livros

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::middleware('auth')->get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('books')->namespace('Books')->name('books.')->group(function () {
+});

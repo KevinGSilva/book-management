7bff8320b5243a2deac2d3a8c4402b24b146837a
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 06:33:16 -0300

    Reposicionando form de livros web

--- resources/views/forms/book.blade.php
+++ resources/views/forms/book.blade.php
@@ -2,33 +2,35 @@
     <div class="card">
         <div class="card-header">Registrar Livro</div>
         <div class="card-body">
-            <div class="col-lg-12">
-                <div class="row mb-3">
-                    <div class="col-lg-4 mb-3">
-                        <label for="title" class="form-label">Título</label>
-                        <input type="text" class="form-control" id="title" name="title" required value="{{ $book->title }}">
-                    </div>
-                    <div class="col-lg-4 mb-3">
-                        <label for="author" class="form-label">Autor</label>
-                        <select class="form-control" id="author" name="author" required>
-                            @foreach ($authors as $author)
-                                <option value="{{ $author->id }}" {{ !empty($book->author->id ) && $book->author->id == $author->id ? 'selected' : '' }}>{{ $author->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-lg-4 mb-3">
-                        <label for="published-at" class="form-label">Ano de publicação</label>
-                        <input type="text" class="form-control date" id="published-at" name="published-at" required value="{{ !empty($book->published_at) ?  $book->published_at->format('d/m/Y') : $book->published_at }}">
-                    </div>
-                    <div class="col-lg-4 mb-3">
-                        <label for="description" class="form-label">Descrição</label>
-                        <textarea class="form-control" id="description" name="description" required>{{ $book->description }}</textarea>
+            <div class="row mb-3">
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-6 mb-3">
+                            <label for="title" class="form-label">Título</label>
+                            <input type="text" class="form-control" id="title" name="title" required value="{{ $book->title }}">
+                        </div>
+                        <div class="col-lg-6 mb-3">
+                            <label for="author" class="form-label">Autor</label>
+                            <select class="form-control" id="author" name="author" required>
+                                @foreach ($authors as $author)
+                                    <option value="{{ $author->id }}" {{ !empty($book->author->id ) && $book->author->id == $author->id ? 'selected' : '' }}>{{ $author->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-lg-6 mb-3">
+                            <label for="published-at" class="form-label">Ano de publicação</label>
+                            <input type="text" class="form-control date" id="published-at" name="published-at" required value="{{ !empty($book->published_at) ?  $book->published_at->format('d/m/Y') : $book->published_at }}">
+                        </div>
+                        <div class="col-lg-6 mb-3">
+                            <label for="description" class="form-label">Descrição</label>
+                            <textarea class="form-control" id="description" name="description" required>{{ $book->description }}</textarea>
+                        </div>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col-lg-3">
-                        <button type="submit" class="btn btn-primary" id="btn-submit">Salvar</button>
-                    </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-lg-3">
+                    <button type="submit" class="btn btn-primary" id="btn-submit">Salvar</button>
                 </div>
             </div>
         </div>

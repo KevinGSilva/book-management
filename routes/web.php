de93f4484c7efa97af98b9e69e25aa7475590742
Kevin Silva <kevincaprino33@gmail.com>
2025-02-01 22:11:52 -0300

    Rota protegida para dashboard web

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware('auth')->get('/', function () {
+    return view('web.dashboard');
+})->name('dashboard');
 
 Auth::routes();
 

d7bc33d3273fbf18361674a736e3cf3b8b583972
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 00:49:49 -0300

    Adicionando jquery cdn no template base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -104,5 +104,7 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         @vite(['resources/js/scripts.js'])
+
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     </body>
 </html>

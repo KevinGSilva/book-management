166524b0eaba016ddf43896e47788cdb63000dec
Kevin Silva <kevincaprino33@gmail.com>
2025-02-02 06:01:28 -0300

    Implementando uso de imagens no model de livros

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Book extends Model
+class Book extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     protected $fillable = [
         'author_id',
         'title',
@@ -20,5 +24,10 @@ class Book extends Model
     public function author()
     {
         return $this->belongsTo(Author::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('cover')->singleFile();
     }
 }
